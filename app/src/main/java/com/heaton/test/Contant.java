ac2a9ab155b8802c5b3af38fb29a4ca152f7b4b3
TangJinHao <1900029908@qq.com>
2018-10-26 11:35:37 +0800

    453

--- app/src/main/java/com/heaton/test/Contant.java
+++ app/src/main/java/com/heaton/test/Contant.java
@@ -9,5 +9,7 @@ import android.Manifest;
 public class Contant {
     public final static  int WRITE_PERMISSION_CODE=110;
     public final static  String WRITE_PERMISSION_TIP ="为了正常使用，请允许读写权限!";
+
+
     public final  static String[] PERMS_WRITE ={Manifest.permission.CALL_PHONE};
 }
